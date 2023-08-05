1505e65c520f3c681ec3bcedf4e8f7d6c0649b6a
Maksim Aptishev GitHub <maximus.testqa777@gmail.com>
2023-08-05 18:23:14 +0300

    add 9

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,5 +1,9 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.codeborne.selenide.selector.ByText;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -7,26 +11,34 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    @BeforeEach
-    void setUpAll() { open("http://localhost:9999/");}
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
-    public void shouldSomethingTest()  {
+    public void shouldSomethingTest() {
         String city = DataGenerator.generateCity();
         String name = DataGenerator.generatorName();
         String phone = DataGenerator.generatePhoneNumber();
-        String planingData = DataGenerator.generateDate(4,"dd.MM.yyyy");
-        String changeDate = DataGenerator.generateDate(8,"dd.MM.yyyy");
-        $("[data-test-id=city] input") .setValue(city);
-        $("[data-test-id=date] input") .doubleClick().sendKeys((planingData));
-        $("[data-test-id=name] input") .setValue(name);
-        $("[data-test-id=phone] input") .setValue(phone);
-        $("[data-test-id=agreement]") .click();
-        $(By.className("button")) .click();
+        String planingData = DataGenerator.generateDate(4, "dd.MM.yyyy");
+        String changeDate = DataGenerator.generateDate(8, "dd.MM.yyyy");
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys((planingData));
+        $("[data-test-id=name] input").setValue(name);
+        $("[data-test-id=phone] input").setValue(phone);
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
         $("[data-test-id=success-notification]").shouldHave(Condition.text("Встреча успешно запланирована на " + planingData));
-        $(By.className("button")) .click();
-        $("[data-test-id=date] input") .doubleClick().sendKeys((changeDate));
-        $(By.className("button")) .click();
+        $(By.className("button")).click();
+        $("[data-test-id=date] input").doubleClick().sendKeys((changeDate));
+        $(By.className("button")).click();
         $(new ByText("Перепланировать")).click();
         $("[data-test-id=success-notification]").shouldHave(Condition.text("Встреча успешно запланирована на " + changeDate));
 
